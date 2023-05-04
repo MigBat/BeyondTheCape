1734efd1af414b4472301ef245854e767f74ceba
Miguel Batista <1211786@isep.ipp.pt>
2023-05-04 15:26:08 +0100

    vanilla v.1.35.2

---
+++ common/defines/difficulty_hard.lua
@@ -0,0 +1,11 @@
+-- Define overrides when playing on Hard
+
+-- Format for overwriting define values:
+--
+-- NDefines.NDiplomacy.MAX_CLIENT_STATES = 20
+
+NDefines.NGame.GREAT_POWER_FRENZY_QUICKSTART = 1
+NDefines.NAI.POWERBALANCE_DISABLE_VERSUS_PLAYER = 0
+NDefines.NAI.GREAT_POWER_ACTIONS_DISABLE_VERSUS_PLAYER = 0
+NDefines.NAI.AI_FORT_PER_DEV_RATIO = 50
+NDefines.NAI.PRESS_THEM_FURTHER = 1
